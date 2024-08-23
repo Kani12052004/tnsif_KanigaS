025e5df8288402229389c851abbb30f99c1db79f
Amudha <Amudha@Amudha-PC>
2024-08-23 12:21:05 +0530

    commit

--- C43_KanigaS/src/com/tnsif_day9/Interfaces/WrapperClasses/WrapperDemo.java
+++ C43_KanigaS/src/com/tnsif_day9/Interfaces/WrapperClasses/WrapperDemo.java
@@ -1,10 +1,20 @@
 package com.tnsif_day9.Interfaces.WrapperClasses;
 
 public class WrapperDemo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Unboxing - Converting explicity
+		Integer i = new Integer(10);
+		System.out.println(i);
+		int b = i.intValue(); //predefined method
+		System.out.println(b);
 
+		// Without using intValue()
+		int c = i;
+		System.out.println(c);
+		
+		//Autoboxing - Converting Implicitly
+		int a = 100;
+		Integer i1 = a;
+		System.out.println(i1);
 	}
-
-}
+	}
